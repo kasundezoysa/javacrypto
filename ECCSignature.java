fde475e8d8b653354bb08c197a5f7ffc3c6c92d9
Kasun De Zoysa <kasundezoysa@users.noreply.github.com>
2023-03-23 19:00:55 +0530

    Add files via upload

--- ECCSignature.java
+++ ECCSignature.java
@@ -1,7 +1,7 @@
 /*
 Kasun De Zoysa @ UCSC
 Compile:
-javac -cp ../BC/bcprov-jdk16-146.jar ECCSignature.java 
+javac -cp ../BC/bcprov-jdk16-146.jar ECCSignature.java
 Execute:
 java -cp ../BC/bcprov-jdk16-146.jar:. ECCSignature
 */
@@ -18,13 +18,11 @@ public class ECCSignature{
 
   public static void main(String[] args)throws Exception {
 
-    
+
      Security.addProvider(new BouncyCastleProvider());
-     
      KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA");
      keyGen.initialize(256,new SecureRandom());
      KeyPair keyPair = keyGen.generateKeyPair();
-
 
      Signature  signature = Signature.getInstance("SHA256withECDSA");
 
@@ -33,11 +31,10 @@ public class ECCSignature{
      String msg1="Hello Kasun How are you?";
      signature.update(msg1.getBytes());
      byte[]  sigBytes = signature.sign();
-     
+
      System.out.println("Signature: ");
      for(byte b:sigBytes) System.out.format("%02x",b);
      System.out.println("");
-
 
      // verify a signature
      signature.initVerify(keyPair.getPublic());
@@ -53,4 +50,3 @@ public class ECCSignature{
   }
 
 }
-
